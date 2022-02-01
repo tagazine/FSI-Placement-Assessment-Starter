470b9ce7eb2f799254724295bb270e6a65919e42
Your Namegit config --global user.name Your <you@example.com>
2022-01-31 19:01:58 -0500

    Comments Commit

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,12 @@ let yourName = "Jeff Horner" // HINT: Replace this with your own name!
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
-let total = 0
+let total = 0 // Total cookies
 
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+
+// buttons
 const gbPlusBtn = document.querySelector('#add-gb')
 const gbMinusBtn = document.querySelector('#minus-gb')
 
@@ -23,6 +24,7 @@ const sugarMinusBtn = document.querySelector('#minus-sugar')
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
+// Functions
 function changeGB(gbTotalDisplay) {
     let gb = document.querySelector('#qty-gb')
     gb.innerHTML = gbTotalDisplay;
@@ -42,7 +44,7 @@ function changeTotal(totalDisplay) {
     let total = document.querySelector('#qty-total')
     total.innerHTML = totalDisplay;
 }
-// Event listener for clicks on the "+" button for Gingerbread cookies
+// Event listeners
 gbPlusBtn.addEventListener('click', function() {
     gb++
     total++
